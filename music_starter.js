5bcd8b60fcf845ffbe46cfa78d88f955d9efb5b3
JCajilog <cajilogj@gmail.com>
2024-09-01 22:09:14 +1200

    DayTwo Changes

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,114 @@
+let fadein = 0
+let ending = 12418 //end of .csv
+let lineHistory = [];
+let linesegment = 50
+let cinemaBar =70
+let raining = [];
+let rainTilt = 2
 
-// vocal, drum, bass, and other are volumes ranging from 0 to 100
+
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
+  let From = color(61,52,69);
+  let To = color(83,67,97);
+  let Heartbeat = lerpColor(From, To, map(other, 70, 80, 0, 1))
+  background(Heartbeat) //IMPORTANT BECAUSE RESET FRAME TO DRAW NEXT
   textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER)
   textSize(24);
-  
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
+  rectMode(CENTER)
+  lineVisual = drum
+  LVisMax = 100
 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+  //RAIN
+  if (counter>0){
+  push();
+  for (let r of raining){ //this helps to target the individual values in the array
+    r.show(bass)
+    r.fall(bass)
+  }
+
+  for (let i=0; i<(map(bass, 0, 100, 1, 3)); i++){
+    raining.push(new Rain(random(width), 0))
+  }
+  pop();
+} 
+
+  //CHARACTER
+  push();  
+  stroke (200, map(counter, 0, 1500, 0,255))
+  fill (255, map(counter, 0, 1500, 0, 255))  //fade in from start
+  rect(map(counter, 0, ending, 1190, 100)-50, height/2, 20, 100)
+  pop();
+
+  //LINE
+  if (counter > 0) { //so line doesn't move while not starting song
+  push();
+    drawingContext.setLineDash([3]);
+    strokeJoin(ROUND)
+    noFill()
+    stroke(200, map(counter, 0, 1000, 0, 255))
+   strokeWeight(4)
+   lineHistory.push(lineVisual); //to change what the line models
+   
+    beginShape();
+   for (let i=0; i<lineHistory.length; i++){
+
+    if (lineHistory.length >= linesegment) { //for the line to cut
+      lineHistory.splice(0, 1);
+      linesegment = linesegment + 0.015
+    }
+
+     let x = map(i, 0, lineHistory.length, 1190, map(counter, 0, ending, 1190, 90));
+     let y =(height/2)+50-map(lineHistory[i], 0, 100, 0, LVisMax);
+
+      vertex (x, y);
+  }
+    endShape();
+    // point (map(counter, 0, ending, 1190, 100)-80 + (100*sin(counter)), (height/2)+50+ (150*sin(counter*5))-20) //THIS MODELS THE CONTROL POINT WHICH ALLOWS THE TRAIL TO MOVE
+    beginShape(); //Line connecting rect() to trail. MIGHT GET RID OF
+    curveVertex (map(counter, 0, ending, 1190, 100)-80 + (100*sin(counter)), (height/2)+50 + (150*sin(counter*5))-20)
+    curveVertex (map(counter, 0, ending, 1190, 100)-40, (height/2)+50)
+    curveVertex (map(counter, 0, ending, 1190, 110), (height/2)+50-map(lineVisual, 0, 100, 0, LVisMax))
+    curveVertex (map(counter, 0, ending, 1190, 110), (height/2)+50-map(lineVisual, 0, 100, 0, LVisMax))
+    endShape();
+  pop();
+  } else {
+    lineHistory = [];  //reset the array when starting song again. 
+  }
+
+   //CinematicBars +ref text
+ push();
+ rectMode(CORNER);
+ stroke(0)
+ fill(0)
+ rect (0,0, 1280, cinemaBar)
+ rect (0,720-cinemaBar, 1280, cinemaBar)
+
+ fill (255,0,0) 
+ text ("1280x720", 10, 30)  //reference text
+ text (counter, 10, 60)
+ pop();
+
 }
+
+//RAIN 
+class Rain {
+  constructor (x, y) {
+    this.x = x - 110
+    this.y = y
+    this.length = random (10, 15) 
+  }
+  show (bass){
+    strokeWeight (random(1, map(bass, 0, 100, 5, 9)))
+    stroke (255, random (40, 130))
+    line (this.x, this.y, this.x, this.y + this.length)
+  }
+  fall(bass){
+    // this.Falling = Falling + random(20, 60)
+    this.y += map(bass, 0, 100, 4, 12)
+    this.x += rainTilt
+    if  (raining.length> map(bass, 0, 100, 247, 297)){
+      raining.splice(0,1);
+    }
+  }
+}
+
--- system_runner.js
+++ system_runner.js
@@ -1,5 +1,5 @@
-const canvasWidth = 540;
-const canvasHeight = 960;
+const canvasWidth = 1280; //HD (High Definition) 720p 16:9  
+const canvasHeight = 720;
 
 
 let mainCanvas;
